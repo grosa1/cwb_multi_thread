216a635c7b43a951a1b91af5c6a7137aa0b982fe
Giovanni Rosa <g.rosa1@studenti.unimol.it>
2019-10-17 00:25:45 +0200

    added res parser

---
+++ parse_results.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+
+parent_f = sys.argv[1]
+
+with open(os.path.join(parent_f, 'cwb_result.csv'), 'w') as out_file:
+    out_file.write('RULE,USER,EXPECTED_RESULT,CURRENT_RESULT\n')
+    for res in os.listdir(parent_f):
+        current = os.path.join(parent_f, res)
+        if os.path.isfile(current) and current.endswith('.txt'):
+            with open(current, 'r') as infile:
+                current_result = 'TRUE' if 'TRUE' in infile.read() else 'FALSE'
+                
+                filename = res.split('.')[0].split('_')
+
+                current_rule = filename[2][1:]
+                current_user = filename[1][1:]
+
+                expected_result = 'TRUE' if current_rule in current_user else 'FALSE'
+
+                out_file.write(current_rule + ',' + current_user + ',' + expected_result + ',' + current_result + '\n')
+
+
