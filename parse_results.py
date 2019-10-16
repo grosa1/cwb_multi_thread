d448c77b20309a662f61ea255f7552a06089091a
Giovanni Rosa <g.rosa1@studenti.unimol.it>
2019-10-17 00:29:59 +0200

    aggiunto check risultati

--- parse_results.py
+++ parse_results.py
@@ -5,7 +5,7 @@ import os
 parent_f = sys.argv[1]
 
 with open(os.path.join(parent_f, 'cwb_result.csv'), 'w') as out_file:
-    out_file.write('RULE,USER,EXPECTED_RESULT,CURRENT_RESULT\n')
+    out_file.write('RULE,USER,EXPECTED_RESULT,CURRENT_RESULT,CHECK\n')
     for res in os.listdir(parent_f):
         current = os.path.join(parent_f, res)
         if os.path.isfile(current) and current.endswith('.txt'):
@@ -18,7 +18,8 @@ with open(os.path.join(parent_f, 'cwb_result.csv'), 'w') as out_file:
                 current_user = filename[1][1:]
 
                 expected_result = 'TRUE' if current_rule in current_user else 'FALSE'
+                check = 'OK' if current_result in expected_result else 'ERROR'
 
-                out_file.write(current_rule + ',' + current_user + ',' + expected_result + ',' + current_result + '\n')
+                out_file.write(current_rule + ',' + current_user + ',' + expected_result + ',' + current_result + ',' + check + '\n')
 
 
